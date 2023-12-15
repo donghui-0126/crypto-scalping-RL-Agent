3c245ec40e54409f5c253cb2757c509a70ef9555
donghui Lim <lukehui@naver.com>
2023-12-15 22:47:54 +0900

    update

---
+++ bitcoin_scalping_bot/version_3/env3.py
@@ -0,0 +1,210 @@
+from collections import deque
+from datetime import datetime
+
+
+class Environment3:
+    # mid price = (high + low) / 2
+    # df[index][col][-1]
+    PCT_IDX = 17 # 1분전 MID price의 가격 변동 
+    MID_IDX = 5
+    
+    def __init__(self, chart_data=None, chart_index:list = None, risk_adverse= 1.3 ,stop_trade=0.9 ,balance=100000000, transaction=0.0004, max_leverage=10):
+        self.chart_data = chart_data # numpy array
+        self.chart_index = chart_index # python list
+        self.idx = 0
+
+        self.risk_adverse = risk_adverse # 손실에 주는 가중치 
+        self.stop_trade = stop_trade # 손절선
+        self.transaction = transaction # 거래수수료
+        self.max_leverage = max_leverage
+        
+        self.current_state = chart_data[self.idx]
+        self.next_state = chart_data[self.idx+1]
+                    
+        self.current_price = self.chart_data[self.idx][self.MID_IDX][-1]
+        self.next_price = self.chart_data[self.idx+1][self.MID_IDX][-1]
+        
+        self.initial_balance = balance
+        self.balance = [balance]  # 포트폴리오가 보유한 현금
+        self.bitcoin = [0]  # 포트폴리오가 보유한 비트코인의 가치 (매 거래마다 바로 청산됨)
+        self.portfolio_value = []
+        
+        self.action_list = deque([5 for i in range(31)]) # 이전 32분을 저장함.
+        self.action_info = [-10, -5, 0, 5, 10]
+        self.position = 0 # 이전의 position 비율을 저장하는 변수. (+)는 long, (-)는 short
+        self.profit_queue = deque([0.0004 for i in range(64)]) # 이전 1시간의 변동성을 고려함
+        
+        
+    def reset(self):
+        self.idx = 0
+        state = self.chart_data[self.idx]
+        return state
+    
+    def get_profit_std(self, profit):
+        self.profit_queue.popleft()
+        self.profit_queue.append(profit)
+        std = (pow(sum(self.profit_queue),2)/len(self.profit_queue))**0.5
+        return abs(std)
+    
+    
+    def step(self, action):
+        self.current_state = self.chart_data[self.idx]
+        self.next_state = self.chart_data[self.idx+1]
+        self.current_price = self.chart_data[self.idx][self.MID_IDX][-1]
+        self.next_price = self.chart_data[self.idx+1][self.MID_IDX][-1]
+
+        current_value = self.balance[-1] + self.bitcoin[-1]*self.current_price 
+        self.portfolio_value.append(current_value)
+        
+        s_prime = self.chart_data[self.idx+1]
+        
+        # action list에 새로운 action 추가해줌
+        self.action_list.append(action)
+        self.action_list.popleft()
+            
+        # reward 계산
+        profit = self.get_reward(action)
+        
+        # 얻은 수익률의 표준편차를 구해준다.
+        std = self.get_profit_std(profit)  
+            
+        # sharpe ratio를 maximize하는 형식
+        reward = profit/std 
+        
+        # risk adverse정도를 고려해서 reward 계산
+        if reward<0:
+            reward =  reward * self.risk_adverse
+        
+        current_time = datetime.strptime(self.chart_index[self.idx], '%Y-%m-%d %H:%M:%S')
+        current_day = datetime.strftime(current_time, '%Y-%m-%d')
+        next_time = datetime.strptime(self.chart_index[self.idx+1], '%Y-%m-%d %H:%M:%S')
+        next_day = datetime.strftime(next_time, '%Y-%m-%d')
+
+        # 시간 index 갱신
+        self.idx += 1
+
+        if current_day == next_day:
+            return {"state_time":current_day, 
+                    "next_state":s_prime, 
+                    "reward":round(reward,8), 
+                    "done":False, 
+                    "portfolio_value":self.portfolio_value[-1], 
+                    "balance":self.balance[-1], 
+                    "bitcoin":self.bitcoin[-1], 
+                    "position":self.position,
+                    "action_list":self.action_list,
+                    "current_price": self.current_price,
+                    "index": self.idx}
+        else:
+            print("#########################################################################")
+            print(f'{current_day}에서 {format(round(self.portfolio_value[-1]), ",")}원으로 trading stop')
+            
+            P = self.portfolio_value[-1]
+            B = self.balance[-1]
+            C = self.bitcoin[-1]
+            
+            # 다음날로 넘어갈 때 포지션 청산 
+            self.position = 0
+            
+            # 다음날 거래는 다시 1억으로 시작
+            self.balance = [self.balance[0]]  # 포트폴리오가 보유한 현금
+            self.bitcoin = [0]  # 포트폴리오가 보유한 비트코인의 가치 (매 거래마다 바로 청산됨)
+            self.portfolio_value = []
+            
+            return {"state_time":current_day, 
+                    "next_state":s_prime, 
+                    "reward":round(reward,8), 
+                    "done":True, 
+                    "portfolio_value":P, 
+                    "balance":B, 
+                    "bitcoin":C, 
+                    "position":self.position,
+                    "action_list":self.action_list,
+                    "current_price": self.current_price,
+                    "index": self.idx}
+    
+    
+    
+    # 로직 다시 점검해야할듯    
+    def position_calc(self, action): # max_leverage를 고려해서 position을 계산해주는 함수
+        action = self.action_info[action] # action의 실제 action
+        execute_position = self.position
+        
+        if action * self.position > 0:
+            if  self.position + action > self.max_leverage:
+                ratio = 0
+                self.position = self.max_leverage
+                return self.position, ratio, False
+            
+            elif self.position + action < -self.max_leverage:
+                ratio  = 0 
+                self.position = -self.max_leverage
+                return self.position, ratio, False
+            else:
+                self.position += action
+                ratio = action
+                return self.position, ratio, False
+                    
+        elif action * self.position < 0:
+            self.position = action 
+            ratio = action 
+            return self.position, ratio, True
+        
+        elif action*self.position==0:
+            if action==0:
+                self.position = action 
+                return self.position, 0, False
+            
+            elif self.position==0:
+                self.position = action 
+                return self.position, self.position, False
+            
+        
+    def get_reward(self, action):     
+        temp_position = self.position
+        
+        # Short
+        if action <= 1:
+            position, ratio, execution = self.position_calc(action)    
+            sell_budget = self.initial_balance * ratio
+            if execution: 
+                # 직전 포지션이 long, clearing_budget: (+)
+                clearing_budget = (self.bitcoin[-1]) * self.current_price
+                # 이전에 매수를 한 경우 => 현재 매도(long 청산)
+                self.balance.append(self.balance[-1] - sell_budget*(1-self.transaction) + clearing_budget*(1-self.transaction)) # clearing budget logic이 오류가 있음.
+                self.bitcoin.append(sell_budget/self.current_price)          
+            else: # 직전 포지션이 short
+                self.balance.append(self.balance[-1] - sell_budget*(1-self.transaction))
+                self.bitcoin.append(self.bitcoin[-1] + sell_budget/self.current_price)  
+        
+            current_value = self.portfolio_value[-1]
+            next_value = self.balance[-1] + self.bitcoin[-1]*(self.next_price)
+            reward = (next_value - current_value) / self.initial_balance
+            return reward
+            
+        # Long
+        elif action >= 3:
+            position, ratio, execution = self.position_calc(action)
+            buy_budget = self.initial_balance * ratio
+    
+            if execution: # 직전 포지션 short
+                clearing_budget = (self.bitcoin[-1]) * self.current_price
+                self.balance.append(self.balance[-1] - buy_budget*(1+self.transaction) + clearing_budget*(1+self.transaction)) # clearing budget logic이 오류가 있음.
+                self.bitcoin.append(buy_budget/self.current_price)              
+
+            else: # 직전 포지션 long
+                self.balance.append(self.balance[-1] - buy_budget*(1+self.transaction))
+                self.bitcoin.append(self.bitcoin[-1] + buy_budget/self.current_price)  
+            
+            current_value = self.portfolio_value[-1]
+            next_value = self.balance[-1] + self.bitcoin[-1]*(self.next_price)
+            reward = (next_value - current_value) / self.initial_balance
+            return reward
+        
+        # HOLD
+        elif action == 2:
+            current_value = self.portfolio_value[-1]
+            next_value = self.balance[-1] + self.bitcoin[-1]*(self.next_price)
+            reward = (next_value - current_value) / self.initial_balance
+            return reward
+        
---
+++ bitcoin_scalping_bot/version_3/main3.py
@@ -0,0 +1,122 @@
+import torch
+from torch.distributions import Categorical 
+from env3 import Environment3
+from ppo3 import PPO3
+import tqdm
+import pandas as pd
+import numpy as np
+import warnings 
+import pickle
+warnings.filterwarnings('ignore')
+
+T_horizon = 128
+
+date_log = []
+value_log = []
+action_log = []
+reward_log = []
+
+def main(model_name, risk_adverse, epochs = 100, transaction=0.0002):
+    with open('bitcoin_scalping_bot\\upbit_data\\train_data_2023_3D.pkl', 'rb') as f:
+        df = pickle.load(f)
+    df_index = pd.read_csv("bitcoin_scalping_bot\\upbit_data\\train_data_2023_3D_index.csv", index_col=0)
+    
+    print("데이터 적재 완료...")
+    if model_name=="ppo3":
+        model = PPO3()
+
+    env = Environment3(df, list(df_index.index), risk_adverse = risk_adverse, transaction=transaction, max_leverage=10)
+    state = env.reset()
+        
+    h1_out = (torch.zeros([1, 1, 64], dtype=torch.float), torch.zeros([1, 1, 64], dtype=torch.float))
+    
+        
+    for n_epi in tqdm.tqdm(range(epochs)):
+        #position_action 이라는 list를 만들어주기위한 임시 변수
+        temp = [5 for x in range(31)] 
+        temp.append(0)
+        position_action = np.array(temp)
+        
+        done = False   
+        date_list = []
+        value_list = []
+        action_list = []
+        reward_list = []
+        balance_list = []
+        coin_list = []
+        position_list = []
+        all_action_list  =[]
+        current_price_list = []
+        index_list = []         
+        while not done:
+            for t in range(T_horizon):
+                h1_in = h1_out
+                
+                prob, h1_out = model.pi(torch.from_numpy(state).float(),h1_in, torch.tensor(position_action))
+                
+                prob = prob.view(-1)
+                
+                action_distribition = Categorical(prob)
+                
+                action = action_distribition.sample().item() # softmax에서 0~11의 값을 return
+                env_step_dict = env.step(action)
+                
+                state_time = env_step_dict["state_time"]
+                next_state = env_step_dict["next_state"]
+                reward = env_step_dict["reward"]
+                done = env_step_dict["done"]  
+                portfolio_value = env_step_dict["portfolio_value"]  
+                balance = env_step_dict["balance"]  
+                bitcoin = env_step_dict["bitcoin"]
+                position = env_step_dict["position"]  
+                all_action = env_step_dict["action_list"] 
+                current_price = env_step_dict["current_price"]
+                index = env_step_dict["index"]
+                
+
+                # log 저장
+                date_list.append(state_time)
+                value_list.append(portfolio_value)
+                action_list.append(action)
+                reward_list.append(reward)
+                balance_list.append(balance)
+                coin_list.append(bitcoin)
+                position_list.append(position)
+                all_action_list.append(all_action)
+                current_price_list.append(current_price)
+                index_list.append(index)
+
+                
+                # position action 병합
+                position_action = list(all_action)
+                position_action.append(position)
+
+                model.put_data([np.array(state, dtype=np.float32),
+                            action, 
+                            reward,
+                            np.array(next_state, dtype=np.float32),\
+                            prob[action].item(),\
+                            h1_in, h1_out, done, position_action])
+
+                state = np.array(next_state, dtype=np.float32)
+
+                if done==True:
+                    break
+                
+            model.train_net()
+
+            log = pd.DataFrame([date_list, value_list, action_list, reward_list, balance_list, coin_list, position_list, current_price_list, index_list]).T
+            log.columns = ["date", "value", "action", "reward", "balance", "coin", "position","price", "index"]
+            log.to_csv("bitcoin_scalping_bot\\version_3\\train_log\\log_{}.csv".format(n_epi+1))
+            
+            if done == True:
+                print("# of episode :{} ".format(n_epi+1))
+                break
+            
+    print("#####")
+    print("END")
+    print("#####")
+    
+
+if __name__ == '__main__':
+    main(model_name="ppo3", risk_adverse=3, epochs=300, transaction=0.000)
